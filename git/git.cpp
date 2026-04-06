00255292d2532cefe1210fbbe865a6a29666288d
Artem <artem.chayka011@gmail.com>
2026-04-06 19:47:39 +0300

    artemm

---
+++ git/git.cpp
@@ -0,0 +1,14 @@
+// git.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+    cout  << "test git class {step 1}" << endl;
+    cout <<  "test git class {step 1}" << endl;
+
+
+}
+
